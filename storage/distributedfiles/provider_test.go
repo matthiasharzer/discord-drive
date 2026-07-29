d9d8d52e0a775e65b0036190381551bf500e8570
Matthias Harzer <matthias.harzer03@gmail.com>
2026-07-29 18:58:39 +0200

    test: add tests for distributedfiles provider

---
+++ storage/distributedfiles/provider_test.go
@@ -0,0 +1,59 @@
+package distributedfiles_test
+
+import (
+	"io"
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/matthiasharzer/discord-drive/storage/distributedfiles"
+	"github.com/matthiasharzer/discord-drive/util/fsutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProvider_Save(t *testing.T) {
+	t.Run("reads a saved file", func(t *testing.T) {
+		directory, cleanup, err := fsutils.TemporaryDirectory()
+		require.NoError(t, err)
+		defer cleanup()
+
+		provider := distributedfiles.NewProvider(int64(8), directory)
+
+		err = provider.Save("testfile.txt", strings.NewReader("abcdefghijkl"))
+		assert.NoError(t, err)
+
+		reader, err := provider.Retrieve("testfile.txt")
+		require.NoError(t, err)
+
+		data, err := io.ReadAll(reader)
+		require.NoError(t, err)
+
+		assert.Equal(t, "abcdefghijkl", string(data))
+	})
+
+	t.Run("distributes content over multiple files", func(t *testing.T) {
+		directory, cleanup, err := fsutils.TemporaryDirectory()
+		require.NoError(t, err)
+		defer cleanup()
+
+		provider := distributedfiles.NewProvider(int64(8), directory)
+
+		err = provider.Save("testfile.txt", strings.NewReader("abcdefghijkl"))
+		assert.NoError(t, err)
+
+		chunkFiles, err := os.ReadDir(path.Join(directory, "testfile.txt"))
+		require.NoError(t, err)
+
+		require.Equal(t, 2, len(chunkFiles))
+
+		file1Bytes, err := os.ReadFile(path.Join(directory, "testfile.txt", chunkFiles[0].Name()))
+		assert.NoError(t, err)
+		file2Bytes, err := os.ReadFile(path.Join(directory, "testfile.txt", chunkFiles[1].Name()))
+		assert.NoError(t, err)
+
+		assert.Equal(t, "abcdefgh", string(file1Bytes))
+		assert.Equal(t, "ijkl", string(file2Bytes))
+	})
+}
